392256ffe9ab5dc0d186118cf7922b725dbd4598
LisaEliassen <lisaeli@hotmail.no>
2022-03-03 16:54:03 +0100

    Implemented communication between server and client sockets.

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
-from getpass import getpass
-from os import environ  # To be discussed in the group session
+from os import environ
 from queue import Queue
 from socket import create_connection, timeout
 from threading import Thread
+from rich import print
 
 
 class Client:
@@ -11,12 +11,50 @@ class Client:
         self._server = server
         self._buffer_size = buffer_size
         self._messages = Queue()
+        self._champs = []
 
     def start(self):
-        if self._register():
+        if self._choose_team():
             self._chatting = True
             Thread(target=self._recv).start()
-            self._chat()
+            self._choose_champions()
+
+    def _choose_champions(self):
+        while (message := input("Choose champion:")).lower() != ".exit":
+            if message:
+                self._sock.sendall(message.encode())
+            while not self._messages.empty():
+                print(self._messages.get())
+        self._chatting = False
+        self._sock.close()
+
+    def _choose_team(self) -> bool:
+        print("Choosing team...")
+        print("Red or Blue? \n")
+        while player := input("Player: "):
+            message = player
+            self._sock = create_connection((self._server, 5550))
+            self._sock.sendall(message.encode())
+            response = self._sock.recv(self._buffer_size).decode()
+            print(response)
+            if response != "Invalid team":
+                return True
+        return False
+
+    def _recv(self):
+        while self._chatting:
+            try:
+                data = self._sock.recv(2048)
+            except timeout:
+                pass
+            except:
+                break
+            else:
+                if data:
+                    self._messages.put(data.decode())
+                else:
+                    break
+
 
 if __name__ == "__main__":
     server = environ.get("SERVER", "localhost")
--- server.py
+++ server.py
@@ -1,15 +1,20 @@
+from os import environ
 from socket import create_server, timeout
 from threading import Lock, Thread
-from os import environ
+from champlistloader import load_some_champs
+from rich import print
+import TLT as TLT
 
 
-class Server:
+class ChatServer:
 
     def __init__(self, host: str, port: int, buffer_size: int = 2048):
         self._host = host
         self._port = port
         self._buffer_size = buffer_size
+        self._players = ["Red", "Blue"]
         self._connections = {}
+        self._player_lock = Lock()
         self._connections_lock = Lock()
 
     def turn_on(self):
@@ -29,8 +34,77 @@ class Server:
         with self._connections_lock:
             return list(self._connections)
 
+    @property
+    def registered_users(self):
+        with self._player_lock:
+            return list(self._players)
+
+    def _accept(self):
+        while self._serving:
+            try:
+                conn, _ = self._welcome_sock.accept()
+            except timeout:
+                pass
+            else:
+                Thread(target=self._welcome, args=(conn,)).start()
+
+    def _welcome(self, conn):
+        if data := conn.recv(self._buffer_size):
+            player = data.decode()
+            if self._choose_team(conn, player):
+                with self._connections_lock:
+                    self._connections[player] = conn
+                while True:
+                    if "Blue" in self._connections and "Red" in self._connections:
+                        break
+
+                conn.sendall("Both players have joined".encode())
+                self._handle_user(conn, player)
+        conn.close()
+
+    def _choose_team(self, conn, player):
+        with self._player_lock:
+            if player in self._players:
+                print(f"Player {player} has joined.")
+                #conn.sendall("Joined".encode())
+                return True
+            conn.sendall("Invalid team".encode())
+            return False
+
+    def _handle_user(self, conn, player):
+
+        champions = load_some_champs()
+        available_champs = TLT.available_champs(champions)
+        welcome_msg = '\n' \
+                      + 'Welcome to [bold yellow]Team Local Tactics[/bold yellow]!' \
+                      + '\n' \
+                      + 'Each player choose a champion each time.' \
+                      + '\n'
+
+        # TO-DO: Use pickle to convert Rich Table into bit strings:
+        #conn.sendall(welcome_msg.encode())
+        #conn.sendall(available_champs)
+
+        while self._serving:
+            if message := conn.recv(self._buffer_size):
+                self._send_from_player(player, message)
+            else:
+                break
+        del self._connections[player]
+
+    def _send_from_player(self, player, message):
+        with self._connections_lock:
+            for key in self._connections:
+                if key != player:
+                    try:
+                        self._connections[key].sendall(
+                            player.encode() + b"| " + message
+                        )
+                    except:
+                        del self._connections[key]
+
 
 if __name__ == "__main__":
     host = environ.get("HOST", "localhost")
-    server = Server(host, 5550)
+    server = ChatServer(host, 5550)
     server.turn_on()
